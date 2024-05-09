f6813a09c39e3eb0045ab93db77b264fd7d62749
maggie-njeri <mkiri00@gmail.com>
2024-05-09 12:51:58 -0700

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -31,24 +31,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
-
-"""
-#!/usr/bin/python3
-Script that queries subscribers on a given Reddit subreddit.
-
-import requests
-
-
-def number_of_subscribers(subreddit):
-   Return the total number of subscribers on a given subreddit""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "0x16.api.advanced:0-subs"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    print(response.status_code)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0"""
