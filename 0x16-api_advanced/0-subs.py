6b92bab233afe7b6e223721a9aa3652c15318251
maggie-njeri <mkiri00@gmail.com>
2024-05-09 12:47:19 -0700

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,46 @@
 #!/usr/bin/python3
+"""Queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
 """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Gets the total subscribers for a given subreddit"""
+    CLIENT_ID = "ty6SjBtdGwuYTBK47cvBmQ"
+    SECRET_KEY = "LEth00St4DuofF2vb8Sgsp-EXbJp2A"
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+    with open('pwd.txt', 'r') as f:
+        pwd = f.read()
+    data = {
+        'grant_type': 'password',
+        'username': 'Educational-Gear-868',
+        'password': pwd
+    }
+    headers = {"User-Agent": "Subs/0.1"}
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+    TOKEN = res.json()['access_token']
+    headers['Authorization'] = 'beare {}'.format(TOKEN)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+
+"""
+#!/usr/bin/python3
 Script that queries subscribers on a given Reddit subreddit.
-"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit"""
+   Return the total number of subscribers on a given subreddit""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "0x16.api.advanced:0-subs"}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -18,4 +51,4 @@ def number_of_subscribers(subreddit):
         subscribers = data['data']['subscribers']
         return subscribers
     else:
-        return 0
+        return 0"""
